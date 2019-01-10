418ad941dcc3533cfa9784a15cb4b7f09366b783
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2019-01-10 20:40:17 +0530

    Create Github.c

---
+++ Github.c
@@ -0,0 +1,25 @@
+what is GitHub?
+
+GitHub Inc. is a web-based hosting service for version control using Git. 
+It is mostly used for computer code. It offers all of the distributed version control and source code
+management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration 
+features such as bug tracking, feature requests, task management, and wikis for every project.
+
+what is GitHub according to me ?
+ for me  GitHub  is a place  where I  right  mai  learning episodes  and enter 
+ I  store my  c  files.
+ 
+ how is GitHub useful?
+  GitHub helps to organise my files in a separate repository 
+  
+what is a repository ? 
+
+  
+
+  
+
+
+
+
+
+
