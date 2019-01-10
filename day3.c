4b441c494a0a77896d07b495723ad8eaee9ed08d
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2019-01-10 15:20:58 +0530

    Create day3.c

---
+++ day3.c
@@ -0,0 +1,9 @@
+How to use call by reference in c?
+ The very first clue i got is to use pointers.
+Why cant we use call by reference in c?
+What is a pointer in c ?
+ Pointers are variables whose values are memory address.From this defination i get a brief idea that pointer provides the addess 
+ to access the value of the variable.
+How to define a pointer?
+ int *countPtr, count;
+ here int is the return type *countPtr is the pointer which points to the value of the variable count.
