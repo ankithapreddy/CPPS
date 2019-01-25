61cad4fe7a649a7453220725c36cd9da566046ef
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2019-01-25 18:32:29 +0530

    Create fraction.c

---
+++ fraction.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct fractions{
+	int x;
+	int y;
+};
+struct fractions input()
+{
+	int q;
+	int num1,num2,den1,den2;
+	printf("Enter the values of the fractions:\n");
+	scanf("%d%d%d%d",&q.num1,&q.num2,&q.den1,&q.den2);
+}
+int main()
+{
+	struct fractions q;
+	int z;
+	z=input();
+}
