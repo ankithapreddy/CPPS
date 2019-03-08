602442023f7a8c32a2adf04a3aa3b5368af3031a
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2019-03-08 18:15:41 +0530

    Create sarray.c

---
+++ sarray.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct fractions
+{
+	int numerator;
+	int denominator;
+};
+void input1(int *n)
+{
+	printf("Enter the number of fractions you want to add:\n");
+	scanf("%d",n);
+}
+void input2(int *n,struct fractions *a[n])
+{
+	int i;
+	printf("Enter the fractions you want to add
+}
+struct fractions compute()
+{
+}
+void output()
+{
+}
+void main()
+{
+	int n;
+	struct fractions a[n];
+	input1(&n);
+	input2(&n,&a[n]);
+}
