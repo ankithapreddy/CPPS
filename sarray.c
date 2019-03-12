7b266184077448e5695dffc9e8de60012f244512
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2019-03-12 18:23:41 +0530

    Update sarray.c

--- sarray.c
+++ sarray.c
@@ -8,29 +8,24 @@ void input1(int *n)
 {
 	printf("Enter the number of fractions you want to add:\n");
 	scanf("%d",n);
-	struct fractions a[n];
 	
 }
-/*void input2(int *n,struct fractions *a[n])
+void input2(int *n,struct fractions a[])
 {
 	int i;
-	printf("Enter the fractions you want to add:");
-	for(i=0;i<n;i++)
+	for(i=0;i<*n;i++)
 	{
-		scanf("%d",&a[i]->numerator);
-		scanf("%d",&a[i]->denominator);
+		printf("Enter the numerators\n");
+		scanf("%d",&a[i].numerator);
+		printf("Enter the denominator\n");
+		scanf("%d",&a[i].denominator);
 	}
-}*/
-struct fractions compute()
-{
 }
-void output()
-{
-}
-void main()
+int main()
 {
 	int n;
-	//struct fractions a[n];
 	input1(&n);
-	//input2(&n,&a[n]);
+	struct fractions a[n];
+	input2(&n,&a[n]);
+	return 0;
 }
