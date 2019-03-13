146ae6e91714050a4d24ce63f132619ae114289a
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2019-03-13 18:31:17 +0530

    Update sarray.c

--- sarray.c
+++ sarray.c
@@ -21,11 +21,26 @@ void input2(int *n,struct fractions a[])
 		scanf("%d",&a[i].denominator);
 	}
 }
+struct fractions compute(int *n,struct fractions a[])
+{
+	int i;
+	for(i=0;i<*n-1;i++)
+	{
+        	a[i].numerator=(a[i].numerator*a[i].denominator+a[i].numerator*a[i].denominator);
+		a[i].denominator=a[i].denominator*a[i].denominator;
+	}
+}
+void output(int *n,struct fractions a[])
+{
+	printf("The sum of two fractions is:%d/%d\n",a[n].numerator,a[n].denominator);
+}
 int main()
 {
-	int n;
+	int n,r;
 	input1(&n);
 	struct fractions a[n];
 	input2(&n,&a[n]);
+	compute(n,&a[n]);
+	output(&n,&a[n]);
 	return 0;
 }
