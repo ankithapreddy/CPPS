f3a572ace75e536ebb8d2b1bd28219f6d5499de2
ankithapreddy <42771852+ankithapreddy@users.noreply.github.com>
2019-04-16 18:07:16 +0530

    Update polynomial.c

--- polynomial.c
+++ polynomial.c
@@ -3,30 +3,36 @@
 struct polynomial{
 	int degree;
 	float coefficient;
+	float x;
 };
-void input1(int *n)
+void input1(int *n,int *x)
 {
-	printf("Enter the number of terms of the polynomial\n");
+	printf("Enter the order of the polynomial\n");
 	scanf("%d",n);
+	printf("Enter the value of the vairable x\n");
+	scanf("%d",x);
 }
-void input2(int *n,struct polynomial p[])
+void input2(int n,struct polynomial p[])
 {
-	for(i=0;i<=n;i++)
-	{
-		printf("Enter the degree of polynomial %d\n",i+1);
-		scanf("%d",&a[i].degree);
-		printf("Enter the coefficient of the polynomial%d\n",i+1);
-		scanf("%d",&a[i].coefficient);
-	}
-
+	int i;
+	printf("Enter the coefficients of the variable\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&p[i].coefficient);
+}
+/*int power(int n,struct polynomial variable,struct polynomial p[])
+{
+	int x,i;
+	
+	for(i=0;i<n;i++)
+	printf("%f%d",x.vaiable,p[i].degree,p[i].coefficient);
 }
 void output()
-{}
+{}*/
 void main()
 {
-	int n;
-	input1(&n);
+	int n,x;
+	input1(&n,&x);
 	struct polynomial p[n];
-	input2(&n,&p);
+	input2(n,p);
 	
 }
